afcfa18ed2d40200b2fbda3141f5c83ed4652568
Paul <wesman687@gmail.com>
2024-07-09 13:50:54 -0400

    Frontend Finished?

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
-import Navbar from './Navbar'
-import { albumsData } from '../assets/assets'
-import Albumitem from './Albumitem'
+import React from "react";
+import Navbar from "./Navbar";
+import { albumsData, songsData } from "../assets/assets";
+import Albumitem from "./Albumitem";
+import Songitem from "./Songitem";
 
 const DisplayHome = () => {
   return (
     <>
       <Navbar />
-      <div className='my-5 font-bold text-2xl'>
-        {albumsData.map((item, index)=> <Albumitem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />)}
+      <div className="my-5 font-bold text-2xl">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <Albumitem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="my-5 font-bold text-2xl">
+        <h1 className="my-5 font-bold text-2xl">Today's Biggest Hits</h1>
+        <div className="flex overflow-auto">
+          {songsData.map((item, index) => (
+            <Songitem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default DisplayHome
+export default DisplayHome;
